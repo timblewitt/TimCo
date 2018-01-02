e9e01239a55f2c31a78805f678e8644c5736291f
Tim Blewitt <timblewitt@hotmail.com>
2018-01-02 17:45:42 +0000

    Add files via upload

--- AD-ADC1/timco-dsc1.ps1
+++ AD-ADC1/timco-dsc1.ps1
@@ -39,7 +39,7 @@ Node $AllNodes.NodeName
 
     xWaitforDisk Disk2
       {
-        DiskNumber = 2
+        DiskId = 2
           RetryIntervalSec = $RetryIntervalSec
           RetryCount = $RetryCount
       }
