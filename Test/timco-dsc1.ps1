6cbee700c1f40bf44e8d2681e7d85044f6ea6cf4
timblewitt <timblewitt@hotmail.com>
2018-01-17 15:20:59 +0000

    App / Web resources

--- Test/timco-dsc1.ps1
+++ Test/timco-dsc1.ps1
@@ -346,3 +346,182 @@ Node $AllNodes.NodeName
 
   }
 } 
+
+Configuration WebServer
+{
+Param 
+  (
+    [string[]]$NodeName,
+
+    [Parameter(Mandatory)]
+    [string[]]$DNSServerAddress,
+	  
+    [Parameter(Mandatory)]
+    [String]$DomainName,
+
+    [Parameter(Mandatory)]
+    [System.Management.Automation.PSCredential]$DomainAdmincreds,
+	  
+    [Parameter(Mandatory)]
+    [String]$OUPath,
+
+    [Int]$RetryCount=20,
+    [Int]$RetryIntervalSec=10
+  )
+
+Import-DscResource -ModuleName PSDesiredStateConfiguration
+Import-DscResource -ModuleName xComputerManagement
+Import-DscResource -ModuleName xNetworking
+Import-DscResource -ModuleName xStorage
+Import-DscResource -ModuleName xPendingReboot
+	
+[System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($DomainAdmincreds.UserName)", $DomainAdminCreds.Password)
+
+$Interface = Get-NetAdapter | Where Name -Like "Ethernet*" | Select-Object -First 1
+$InterfaceAlias = $($Interface.Name)
+	
+Node $AllNodes.NodeName
+  {
+    LocalConfigurationManager            
+      {            
+        ActionAfterReboot = 'ContinueConfiguration'            
+        ConfigurationMode = 'ApplyOnly'            
+        RebootNodeIfNeeded = $true      
+		AllowModuleOverWrite = $true      
+      } 
+
+    xWaitforDisk Disk2
+      {
+        DiskId = 2
+        RetryIntervalSec = $RetryIntervalSec
+        RetryCount = $RetryCount
+      }
+
+    xDisk FVolume
+      {
+        DiskId = 2
+        DriveLetter = 'F'
+      }
+	  
+    File CreateFile 
+      {
+        DestinationPath = 'F:\Software\Readme.txt'
+        Ensure = "Present"
+        Contents = 'Store all software in this folder.'
+        DependsOn = "[xDisk]FVolume"
+      }
+	  
+    WindowsFeature TelnetClient
+      {
+        Ensure = 'Present'
+        Name = 'Telnet-Client'
+      }
+
+	WindowsFeature IIS 
+    { 
+        Ensure = "Present" 
+        Name = "Web-Server" 
+        DependsOn = "[xDisk]FVolume"                      
+    } 
+
+    xDnsServerAddress DNSServer
+      {
+        Address = $DNSServerAddress
+        InterfaceAlias = $InterfaceAlias
+        AddressFamily  = "IPv4"
+        DependsOn = "[WindowsFeature]IIS"
+      }
+	  
+    xPendingReboot Reboot1
+      { 
+        Name = "RebootServer"
+        DependsOn = "[xDnsServerAddress]DNSServer"
+      }	
+  }
+} 
+
+Configuration AppServer
+{
+Param 
+  (
+    [string[]]$NodeName,
+
+    [Parameter(Mandatory)]
+    [string[]]$DNSServerAddress,
+	  
+    [Parameter(Mandatory)]
+    [String]$DomainName,
+
+    [Parameter(Mandatory)]
+    [System.Management.Automation.PSCredential]$DomainAdmincreds,
+	  
+    [Parameter(Mandatory)]
+    [String]$OUPath,
+
+    [Int]$RetryCount=20,
+    [Int]$RetryIntervalSec=10
+  )
+
+Import-DscResource -ModuleName PSDesiredStateConfiguration
+Import-DscResource -ModuleName xComputerManagement
+Import-DscResource -ModuleName xNetworking
+Import-DscResource -ModuleName xStorage
+Import-DscResource -ModuleName xPendingReboot
+	
+[System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($DomainAdmincreds.UserName)", $DomainAdminCreds.Password)
+
+$Interface = Get-NetAdapter | Where Name -Like "Ethernet*" | Select-Object -First 1
+$InterfaceAlias = $($Interface.Name)
+	
+Node $AllNodes.NodeName
+  {
+    LocalConfigurationManager            
+      {            
+        ActionAfterReboot = 'ContinueConfiguration'            
+        ConfigurationMode = 'ApplyOnly'            
+        RebootNodeIfNeeded = $true      
+		AllowModuleOverWrite = $true      
+      } 
+
+    xWaitforDisk Disk2
+      {
+        DiskId = 2
+        RetryIntervalSec = $RetryIntervalSec
+        RetryCount = $RetryCount
+      }
+
+    xDisk FVolume
+      {
+        DiskId = 2
+        DriveLetter = 'F'
+      }
+	  
+    File CreateFile 
+      {
+        DestinationPath = 'F:\Software\Readme.txt'
+        Ensure = "Present"
+        Contents = 'Store all software in this folder.'
+        DependsOn = "[xDisk]FVolume"
+      }
+	  
+    WindowsFeature TelnetClient
+      {
+        Ensure = 'Present'
+        Name = 'Telnet-Client'
+      }
+	  
+    xDnsServerAddress DNSServer
+      {
+        Address = $DNSServerAddress
+        InterfaceAlias = $InterfaceAlias
+        AddressFamily  = "IPv4"
+        DependsOn = "[xDisk]FVolume"
+      }
+	  
+    xPendingReboot Reboot1
+      { 
+        Name = "RebootServer"
+        DependsOn = "[xDnsServerAddress]DNSServer"
+      }	
+  }
+} 
