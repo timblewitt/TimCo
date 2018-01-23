0f063b4cc168e4828c3ac581535046cb67e6cdca
timblewitt <timblewitt@hotmail.com>
2018-01-23 16:06:10 +0000

    BB1

---
+++ TestGroiups.ps1
@@ -0,0 +1,9 @@
+﻿$GroupData = @'
+GroupName,Category,GroupScope,Path,MembersToInclude,Description
+G_SysAdmins,Security,Global,"OU=SysAdmins,OU=Users,OU=Managed","AA1,BB1","Global group for SysAdmins"
+G_AppAdmins,Security,Global,"OU=AppAdmins,OU=Users,OU=Managed","CC1","Global group for AppAdmins"
+G_AppUsers,Security,Global,"OU=AppUsers,OU=Users,OU=Managed","FF1","Global group for AppUsers"
+'@
+
+$Groups = $GroupData | ConvertFrom-Csv
+$Groups
