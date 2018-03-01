03144e443899aabbf3b647d7b0642e3d8b9df442
timblewitt <timblewitt@hotmail.com>
2018-03-01 17:10:36 +0000

    Even more basic ps1 file

--- Build-AD-Domain/timco-dsc2.ps1
+++ Build-AD-Domain/timco-dsc2.ps1
@@ -2,22 +2,7 @@ Configuration WebServer
 {
 Param 
   (
-    [string[]]$NodeName,
-
-    [Parameter(Mandatory)]
-    [string[]]$DNSServerAddress,
-	  
-    [Parameter(Mandatory)]
-    [String]$DomainName,
-
-    [Parameter(Mandatory)]
-    [String]$DomainJoin,
-
-    [Parameter(Mandatory)]
-    [System.Management.Automation.PSCredential]$DomainAdmincreds,
-	  
-    [Int]$RetryCount=30,
-    [Int]$RetryIntervalSec=60
+    [string[]]$NodeName
   )
 
 Node $AllNodes.NodeName
